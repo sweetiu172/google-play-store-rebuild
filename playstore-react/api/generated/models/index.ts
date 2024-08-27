a53233e93835ffe0686b52e9e904938258b1af1d
mythdra <nguyencan878@gmail.com>
2024-08-27 16:31:54 +0700

    Remove generated API and model files

--- playstore-react/api/generated/models/index.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './CreateUserRequest';
-export * from './LoginRequest';
-export * from './LoginResponse';
-export * from './UpdateUserRequest';
-export * from './User';
-export * from './UserList';
