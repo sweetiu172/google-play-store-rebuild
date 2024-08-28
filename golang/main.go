b00e2ef162a91d00cee3f64f5af3dea1ba3b288b
mythdra <nguyencan878@gmail.com>
2024-08-29 01:28:36 +0700

    Add new files and directories

---
+++ golang/main.go
@@ -0,0 +1,25 @@
+package main
+
+// @title          Swagger Example API
+// @version        1.0
+// @description    This is a sample server celler server.
+// @termsOfService http://swagger.io/terms/
+
+// @host     localhost:8080
+// @BasePath /api/v1
+
+// @securityDefinitions.apikey Bearer
+// @in                         header
+// @name                       Authorization
+// @description                Type "Bearer" followed by a space and JWT token.
+func main() {
+	// This is the main entry point for the application
+	// It will be called when the application is started
+	// and will be the first function to run
+	// It will be responsible for setting up the application
+	// and starting the main loop
+	// It will also be responsible for handling any errors
+	// that occur during the application's execution
+	// and for cleaning up any resources that the application
+	// has allocated
+}
